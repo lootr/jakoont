331e6baa02da680b9848d0dcdb96ebf0435de06c
Alexandre PIGNÉ <bloodaille@free.fr>
2011-03-29 15:53:43 +0200

    Split project component from app root component

--- jakoont/app.py
+++ jakoont/app.py
@@ -1,71 +1,20 @@
 from __future__ import with_statement
 
-import os
-import itertools
-from nagare import presentation, var
+from nagare import presentation
 from nagare.database import session
-from sqlalchemy.sql import func
 
-from jakoont.models import Project, User, Entry
+from jakoont.models import Project as DBProject
+from jakoont.project import Project
 
 class Jakoont(object):
     def __init__(self):
-        self.project = session.query(Project).first()
-        self.all_users = session.query(User).all()
-        self.entry_users = session.query(User)\
-            .join(Entry)\
-            .filter(Entry.project_id == self.project.id)\
-            .all()
-        self.entry_user_id = var.Var()
+        self.projects = session.query(DBProject).all()
 
-    def add_entry(self, amount):
-        session.add(Entry(amount=amount,
-                          user_id=self.entry_user_id(),
-                          project=self.project))
-    
-    def get_avg_amount(self):
-        avg_amount = session.query(func.sum(Entry.amount))\
-            .filter_by(project_id=self.project.id)\
-            .first()[0]
-        if avg_amount is not None:
-            return avg_amount / len(self.entry_users)
-    
-    def get_users_amounts(self):
-        d = {}
-        for user in self.entry_users:
-            d[user] = session.query(func.sum(Entry.amount))\
-                .filter_by(project_id=self.project.id)\
-                .filter_by(user_id=user.id)\
-                .first()[0]
-        return d
-
-    def get_user_repartition(self):
-        avg_amount = self.get_avg_amount()
-        users_amounts = self.get_users_amounts()
-        d = {}
-        d2 = None
-        while d2 != d:
-            d2 = d.copy()
-            for u1, u2 in itertools.product(self.entry_users, repeat=2):
-                if u1 == u2: continue
-                u1a = users_amounts[u1]
-                u2a = users_amounts[u2]
-                if u1a > avg_amount or u2a < avg_amount or u1a > u2a: continue
-                to_give = u2a - avg_amount
-                if to_give > u1a:
-                    to_give = u1a
-                to_give = round(to_give, 2)
-                if to_give:
-                    users_amounts[u1] += to_give
-                    users_amounts[u2] -= to_give
-                    d[u1][u2] = d.setdefault(u1, {}).setdefault(u2, 0.) + to_give
-        return d
-    
     def login(self):
         pass
 
-    def remove_entry(self, eid):
-        session.query(Entry).filter_by(id=eid).delete()
+    def edit(self, comp, pid):
+        return comp.becomes(Project(pid), "edit")
 
 @presentation.render_for(Jakoont)
 def render(self, h, comp, *args):
@@ -75,6 +24,7 @@ def render(self, h, comp, *args):
         h << comp.render(h, "header")
         h << comp.render(h, "middle")
         h << comp.render(h, "footer")
+        pass
 
     return h.root
 
@@ -94,37 +44,7 @@ def render(self, h, *args):
 
 @presentation.render_for(Jakoont, "middle")
 def render(self, h, comp, *args):
-    h << h.h1(self.project.name)
-    with h.form:
-        with h.table:
-            with h.thead:
-                h << h.th("User")
-                h << h.th("Amount")
-            with h.tbody:
-                for entry in self.project.entries:
-                    with h.tr:
-                        h << h.td(entry.user.username)
-                        h << h.td(entry.amount)
-                        h << h.td(h.a("Remove").action(lambda eid=entry.id: self.remove_entry(eid)))
-                with h.tr:
-                    with h.td:
-                        with h.select(class_="userlist").action(self.entry_user_id):
-                            for user in self.all_users:
-                                h << h.option(user.username, value=user.id)
-                    h << h.td(h.input().action(self.add_entry))
-                    h << h.td(h.input(type_="submit"))
-    
-    avg_amount = self.get_avg_amount()
-    ur = self.get_user_repartition()
-    if avg_amount is not None:
-        with h.p:
-            h << u"Average amount is %.2f, " % avg_amount
-        with h.ul:
-            for u1 in ur:
-                h << h.li(u"%s donne:" %u1.username)
-                with h.ul:
-                    for u2, amount in ur[u1].items():
-                        h << h.li("%.2f a %s" %(amount, u2.username))
+    h << h.ul(h.li(h.a(proj.name).action(lambda pid=proj.id: self.edit(comp, pid))) for proj in self.projects)
     return h.root
 
 # ---------------------------------------------------------------
---
+++ jakoont/project.py
@@ -0,0 +1,113 @@
+from __future__ import with_statement
+
+import itertools
+from nagare import presentation, var
+from nagare.database import session
+from sqlalchemy.sql import func
+
+from jakoont.models import Project as DBProject,\
+    User as DBUser, Entry as DBEntry
+
+class Project(object):
+    def __init__(self, project_id):
+        self.project_id = project_id
+        self.initialize()
+
+    @property
+    def _v_project(self):
+        return session.query(DBProject).get(self.project_id)
+
+    def initialize(self):
+        self.entry_users = session.query(DBUser)\
+            .join(DBEntry)\
+            .filter(DBEntry.project_id == self.project_id)\
+            .all()
+        self.avg_amount = session.query(func.sum(DBEntry.amount))\
+            .filter_by(project_id=self.project_id)\
+            .first()[0]
+        if self.avg_amount is not None:
+            self.avg_amount /= len(self.entry_users)
+        self.users_amounts = dict(
+            (user.username, session.query(func.sum(DBEntry.amount))\
+                 .filter_by(project_id=self.project_id)\
+                 .filter_by(user_id=user.id)\
+                 .first()[0])
+            for user in self.entry_users
+            )
+        self.entry_user_id = var.Var()
+
+    def login(self):
+        pass
+
+    def add_entry(self, amount):
+        session.add(DBEntry(amount=amount,
+                            user_id=self.entry_user_id(),
+                            project_id=self.project_id))
+        self.initialize()
+    
+    def remove_entry(self, eid):
+        session.query(DBEntry).filter_by(id=eid).delete()
+        self.initialize()
+
+    def get_all_users(self):
+        return session.query(DBUser).all()
+    
+    def get_user_repartition(self, precision=2):
+        users_amounts = self.users_amounts.copy()
+        d = {}
+        d2 = None
+        while d2 != d:
+            d2 = d.copy()
+            for u1, u2 in itertools.product(self.entry_users, repeat=2):
+                if u1 == u2: continue
+                u1a = users_amounts[u1.username]
+                u2a = users_amounts[u2.username]
+                if u1a > self.avg_amount or u2a < self.avg_amount or u1a > u2a: continue
+                to_give = u2a - self.avg_amount
+                if to_give > u1a:
+                    to_give = u1a
+                to_give = round(to_give, precision)
+                if to_give:
+                    users_amounts[u1.username] += to_give
+                    users_amounts[u2.username] -= to_give
+                    d[u1.username][u2.username] = d.setdefault(u1.username, {})\
+                        .setdefault(u2.username, 0.) + to_give
+        return d
+
+@presentation.render_for(Project, "edit")
+def render(self, h, comp, *args):
+    project = self._v_project
+    h << h.h1(project.name)
+    with h.form:
+        with h.table:
+            with h.thead:
+                h << h.th("User")
+                h << h.th("Amount")
+            with h.tbody:
+                for entry in project.entries:
+                    with h.tr:
+                        h << h.td(entry.user.username)
+                        h << h.td(entry.amount)
+                        h << h.td(h.a("Remove").action(lambda eid=entry.id: self.remove_entry(eid)))
+                with h.tr:
+                    with h.td:
+                        with h.select(class_="userlist").action(self.entry_user_id):
+                            for user in self.get_all_users():
+                                h << h.option(user.username, value=user.id)
+                    h << h.td(h.input().action(self.add_entry))
+                    h << h.td(h.input(type_="submit"))
+    
+    precision = 2
+    if self.avg_amount is not None:
+        h << h.p(u"Average amount is %.2f, " % self.avg_amount)
+        with h.ul:
+            while precision >= 0:
+                with h.li:
+                    h << u"a %d decimales pres" %precision
+                    with h.ul:
+                        for u1, u2s in self.get_user_repartition(precision).items():
+                            h << h.li(u"%s donne:" %u1)
+                            h << h.ul(h.li("%.2f a %s" %(amount, u2)) for u2, amount in u2s.items())
+                precision -= 1
+
+    return h.root
